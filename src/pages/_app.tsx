168ebe182fa5ce951820b7b7b6f1f17ddc5b40ba
wan2daaa <wan2daaa@gmail.com>
2023-08-24 00:38:49 +0900

    feat: title 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <GlobalFonts />
       <Head>
+        <title>4983</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <GlobalStyle />

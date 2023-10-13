8f049aae8286d88ed96a902d94fffc0e1f3e55ec
wan2daaa <wan2daaa@gmail.com>
2023-10-13 16:28:43 +0900

    fix: api 경로 수정

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const runtimeCaching = require("next-pwa/cache.js");
 const isProduction = process.env.NODE_ENV === "production";
 
 const withPWA = require("next-pwa")({
-  // dest: "public",
-  disable: false,
+  dest: "public",
+  register: true,
+  skipWaiting: true,
   runtimeCaching,
   maximumFileSizeToCacheInBytes: 1000000000,
 });
@@ -31,70 +32,70 @@ module.exports = withPWA({
     return [
       {
         source: "/api/v1/login",
-        destination: "https://dev.4983.link/api/v1/login",
+        destination: "http://localhost:8080/api/v1/login",
       },
       /* 판매글 등록 */
       {
         source: "/api/v1/used-book",
-        destination: "https://dev.4983.link/api/v1/used-book",
+        destination: "http://localhost:8080/api/v1/used-book",
       },
       /* 판매글 수정 */
       {
         source: "/api/v1/used-book/:id",
-        destination: "https://dev.4983.link/api/v1/used-book/:id",
+        destination: "http://localhost:8080/api/v1/used-book/:id",
       },
       /* Chatbot */
       {
         source: "/api/v1/chat-room",
-        destination: "https://dev.4983.link/api/v1/chat-room",
+        destination: "http://localhost:8080/api/v1/chat-room",
       },
       {
         source: "/api/v1/chat/:chatRoomId",
-        destination: "https://dev.4983.link/api/v1/chat/:chatRoomId",
+        destination: "http://localhost:8080/api/v1/chat/:chatRoomId",
       },
       /* Main */
       {
         source: "/api/v1/used-book-list",
-        destination: "https://dev.4983.link/api/v1/used-book-list",
+        destination: "http://localhost:8080/api/v1/used-book-list",
       },
       {
         source: "/api/v1/used-book-list/college-and-department",
         destination:
-          "https://dev.4983.link/api/v1/used-book-list/college-and-department",
+          "http://localhost:8080/api/v1/used-book-list/college-and-department",
       },
       {
         source: "/api/v1/register/duplicate/studentId",
         destination:
-          "https://dev.4983.link/api/v1/register/duplicate/studentId",
+          "http://localhost:8080/api/v1/register/duplicate/studentId",
       },
       {
         source: "/api/v1/register/duplicate/nickname",
-        destination: "https://dev.4983.link/api/v1/register/duplicate/nickname",
+        destination: "http://localhost:8080/api/v1/register/duplicate/nickname",
       },
       {
         source: "/api/v1/my-pages/certification-number",
         destination:
-          "https://dev.4983.link/api/v1/my-pages/certification-number",
+          "http://localhost:8080/api/v1/my-pages/certification-number",
       },
       /* View */
       {
         source: "/api/v1/used-book/:usedBookId",
-        destination: "https://dev.4983.link/api/v1/used-book/:usedBookId",
+        destination: "http://localhost:8080/api/v1/used-book/:usedBookId",
       },
       /* 게시물 개별이미지 삭제 */
       {
         source: "/api/v1/used-book/:usedBookId/image/:imageName",
         destination:
-          "https://dev.4983.link/api/v1/used-book/:usedBookId/image/:imageName",
+          "http://localhost:8080/api/v1/used-book/:usedBookId/image/:imageName",
       },
       /* Chat */
       {
         source: "/api/v1/chat/list",
-        destination: "https://dev.4983.link/api/v1/chat/list",
+        destination: "http://localhost:8080/api/v1/chat/list",
       },
       {
         source: "/api/v1/register",
-        destination: "https://dev.4983.link/api/v1/register",
+        destination: "http://localhost:8080/api/v1/register",
       },
     ];
   },

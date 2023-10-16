165af897bc1aae971b41e73b46e372fd1f7f95fd
zoodaa <jkj01230@gmail.com>
2023-10-16 22:10:04 +0900

    feat: api 구현

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const withPWA = require("next-pwa")({
   dest: "public",
-  register: true,
-  skipWaiting: true,
+  // disable: !isProduction,
   runtimeCaching,
-  maximumFileSizeToCacheInBytes: 1000000000,
 });
 
 module.exports = withPWA({
@@ -32,71 +30,79 @@ module.exports = withPWA({
     return [
       {
         source: "/api/v1/login",
-        destination: "http://15.165.173.111:8080/api/v1/login",
+        destination: "http://15.165.173.111/api/v1/login",
       },
       /* 판매글 등록 */
       {
         source: "/api/v1/used-book",
-        destination: "http://15.165.173.111:8080/api/v1/used-book",
+        destination: "http://15.165.173.111/api/v1/used-book",
       },
-      /* 판매글 수정 */
-      {
-        source: "/api/v1/used-book/:id",
-        destination: "http://15.165.173.111:8080/api/v1/used-book/:id",
-      },
-      /* Chatbot */
       {
         source: "/api/v1/chat-room",
-        destination: "http://15.165.173.111:8080/api/v1/chat-room",
+        destination: "http://15.165.173.111/api/v1/chat-room",
       },
       {
         source: "/api/v1/chat/:chatRoomId",
-        destination: "http://15.165.173.111:8080/api/v1/chat/:chatRoomId",
+        destination: "http://15.165.173.111/api/v1/chat/:chatRoomId",
       },
       /* Main */
       {
         source: "/api/v1/used-book-list",
-        destination: "http://15.165.173.111:8080/api/v1/used-book-list",
-      },
-      {
-        source: "/api/v1/used-book-list/college-and-department",
-        destination:
-          "http://15.165.173.111:8080/api/v1/used-book-list/college-and-department",
+        destination: "http://15.165.173.111/api/v1/used-book-list",
       },
       {
         source: "/api/v1/register/duplicate/studentId",
         destination:
-          "http://15.165.173.111:8080/api/v1/register/duplicate/studentId",
+          "http://15.165.173.111/api/v1/register/duplicate/studentId",
       },
       {
         source: "/api/v1/register/duplicate/nickname",
-        destination:
-          "http://15.165.173.111:8080/api/v1/register/duplicate/nickname",
+        destination: "http://15.165.173.111/api/v1/register/duplicate/nickname",
       },
       {
         source: "/api/v1/my-pages/certification-number",
         destination:
-          "http://15.165.173.111:8080/api/v1/my-pages/certification-number",
+          "http://15.165.173.111/api/v1/my-pages/certification-number",
       },
       /* View */
       {
         source: "/api/v1/used-book/:usedBookId",
-        destination: "http://15.165.173.111:8080/api/v1/used-book/:usedBookId",
+        destination: "http://15.165.173.111/api/v1/used-book/:usedBookId",
       },
       /* 게시물 개별이미지 삭제 */
       {
         source: "/api/v1/used-book/:usedBookId/image/:imageName",
         destination:
-          "http://15.165.173.111:8080/api/v1/used-book/:usedBookId/image/:imageName",
+          "http://15.165.173.111/api/v1/used-book/:usedBookId/image/:imageName",
       },
       /* Chat */
       {
         source: "/api/v1/chat/list",
-        destination: "http://15.165.173.111:8080/api/v1/chat/list",
+        destination: "http://15.165.173.111/api/v1/chat/list",
       },
       {
         source: "/api/v1/register",
-        destination: "http://15.165.173.111:8080/api/v1/register",
+        destination: "http://15.165.173.111/api/v1/register",
+      },
+        /* 마이페이지 판매내역 List */
+      {
+        source: "/api/v1/my-pages/book-sales-detail-list",
+        destination: "http://15.165.173.111/api/v1/my-pages/book-sales-detail-list",
+      },
+      /* 마이페이지 구매내역 List */
+      {
+        source: "/api/v1/my-pages/book-purchase-detail-list",
+        destination: "http://15.165.173.111/api/v1/my-pages/book-purchase-detail-list",
+      },
+      /* 로그아웃 */
+      {
+        source: "/api/v1/withdraw",
+        destination: "http://15.165.173.111/api/v1/withdraw",
+      },
+      /* 회원탈퇴 */
+      {
+        source: "/api/v1/logout",
+        destination: "http://15.165.173.111/api/v1/logout",
       },
     ];
   },
---
+++ src/apis/user/User.tsx
@@ -0,0 +1,82 @@
+import axios from "axios";
+
+/* 마이페이지 판매내역 List */
+export const SalesHistoryList = (canBuy: boolean) =>
+    axios
+        .get(
+            `/api/v1/my-pages/book-sales-detail-list?canBuy=${canBuy}`,
+            {
+                headers: {
+                    Authorization: sessionStorage.getItem("accessToken"),
+                },
+            },
+        )
+        .then(response => response.data)
+        .catch(error => {
+            console.log("실패:", error.response.data);
+            throw error;
+        });
+
+/* 마이페이지 구매내역 List */
+export const PurchaseHistoryList = (canBuy: boolean) =>
+    axios
+        .get(
+            `/api/v1/my-pages/book-sales-detail-list?canBuy=${canBuy}`,
+            {
+                headers: {
+                    Authorization: sessionStorage.getItem("accessToken"),
+                },
+            },
+        )
+        .then(response => response.data)
+        .catch(error => {
+            console.log("실패:", error.response.data);
+            throw error;
+        });
+
+/* 로그아웃 */
+export const UserLogout = () => {
+    axios
+        .get(
+            `/api/v1/logout`,
+            {
+                headers: {
+                    Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+                },
+            },
+        )
+        .then(response => {
+            console.log("로그아웃 성공: ", response.data);
+            sessionStorage.removeItem("accessToken");
+            window.location.href = `/signin`;
+            return response.data;
+        })
+        .catch(error => {
+            console.log("로그아웃 실패: ", error.response.data);
+            throw error;
+        });
+};
+
+/* 화원 탈퇴 */
+export const UserSecede = () => {
+  axios
+    .patch(
+      "/api/v1/withdraw",
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+        },
+      },
+    )
+    .then(response => {
+      console.log("탈퇴 성공: ", response.data);
+      sessionStorage.removeItem("accessToken");
+      window.location.href = `/signin`;
+      return response.data;
+    })
+    .catch(error => {
+      console.log("탈퇴 실패: ", error.response.data);
+      throw error;
+    });
+};

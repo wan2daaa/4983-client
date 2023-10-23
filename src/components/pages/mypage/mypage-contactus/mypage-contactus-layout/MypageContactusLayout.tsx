14f93c733efc210ba9f19a679e8b7735d8919db1
zoodaa <jkj01230@gmail.com>
2023-10-23 18:44:23 +0900

    feat:  문의하기 페이지 뒤로가기 버튼 path 구현

--- src/components/pages/mypage/mypage-contactus/mypage-contactus-layout/MypageContactusLayout.tsx
+++ src/components/pages/mypage/mypage-contactus/mypage-contactus-layout/MypageContactusLayout.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import * as style from "@/components/pages/mypage/mypage-contactus/mypage-contactus-layout/MypageContactusLayout.style";
 import MypageContactusButton from "@/components/pages/mypage/mypage-contactus/mypaeg-contactus-button/MypageContactusButton";
 
 export default function MypageContactusLayout() {
+  const [targetPath, setTargetPath] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    const path = accessToken ? "/mypage" : "/findpassword";
+    setTargetPath(path);
+  }, []);
+
   return (
     <style.Div>
       <style.TitleDiv>
-        <Link href="/mypage">
+        <Link href={targetPath}>
           <style.TitleBackButton />
         </Link>
         <style.Title>문의하기</style.Title>
